cda748cba1c5f7653b6b2d28251ac5fe52b65e2b
novaeeken <nova_eeken@hotmail.com>
2022-11-29 17:42:50 +0100

    Gebruik de filtermethode om over alle objecten in de lijst te mappen

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -12,6 +12,10 @@ function ForecastTab({ coordinates }) {
       try {
           const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&lang=nl`);
           console.log(response.data);
+
+          const fiveDayForecast = response.data.list.filter(() => {
+            return `<CONDITIE BESCHRIJF JE HIER>`
+          });
       } catch(e) {
           console.error(e);
       }
