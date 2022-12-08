cac106a6eeb8baeff00179716134f0bb02271d18
novaeeken <nova_eeken@hotmail.com>
2022-12-08 21:07:41 +0100

    Schrijf een switch statement die op basis van het type het juiste SVG icoon teruggeeft

--- src/helpers/iconMapper.js
+++ src/helpers/iconMapper.js
@@ -7,7 +7,24 @@ import { ReactComponent as Wind } from '../assets/icons/wind.svg';
 import { ReactComponent as Drizzle } from '../assets/icons/sun-rain.svg';
 
 function iconMapper(weatherType) {
-
+  switch (weatherType) {
+    case 'Clear':
+      return <Sunny/>;
+    case 'Clouds':
+      return <Clouds/>;
+    case 'Drizzle':
+      return <Drizzle/>;
+    case 'Rain':
+      return <Rain/>;
+    case 'Snow':
+      return <Snow/>;
+    case 'Mist':
+    case 'Haze':
+    case 'Smoke':
+    case 'Fog':
+    default:
+      return <Wind/>;
+  }
 }
 
 export default iconMapper;
