3aa7f7a6e8267556ce537e3e33811e57938c848b
novaeeken <nova_eeken@hotmail.com>
2022-12-08 21:06:34 +0100

    Importeer react en alle icoontjes

---
+++ src/helpers/iconMapper.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as Sunny } from '../assets/icons/sun.svg';
+import { ReactComponent as Rain } from '../assets/icons/rain.svg';
+import { ReactComponent as Clouds } from '../assets/icons/clouds.svg';
+import { ReactComponent as Snow } from '../assets/icons/snow.svg';
+import { ReactComponent as Wind } from '../assets/icons/wind.svg';
+import { ReactComponent as Drizzle } from '../assets/icons/sun-rain.svg';
+
